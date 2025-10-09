f0fe4e6fd9b27df289938f13be6274c3e1830194
Adrian Rojek <arojek@future-processing.com>
2025-10-09 19:05:00 +0200

    Added default user

--- src/db/supabase.client.ts
+++ src/db/supabase.client.ts
@@ -6,3 +6,7 @@ const supabaseUrl = import.meta.env.SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.SUPABASE_KEY;
 
 export const supabaseClient = createClient<Database>(supabaseUrl, supabaseAnonKey);
+
+export type SupabaseClient = typeof supabaseClient;
+
+export const DEFAULT_USER_ID = "872e74cc-521b-4ddf-955b-bad07835a877";
